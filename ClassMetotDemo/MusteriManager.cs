eb56aca3807a3d25f96397db9705ad67bb1c2240
akman <akman@DESKTOP-L52JTMN>
2021-01-31 14:04:45 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void List(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Adı: "+musteri.Adi+" "+musteri.Soyadi);
+            Console.WriteLine("Hesap Türü: " + musteri.HesapTuru);
+
+        }
+
+        public void Add(string musteriAdi, string musteriSoyadi, string hesapTuru)
+        {
+            Console.WriteLine("Tebrikler! Eklenen müşteri: "+musteriAdi+" "+musteriSoyadi);
+        }
+
+        public void Delete(string musteriAdi, string musteriSoyadi, string hesapTuru)
+        {
+            Console.WriteLine("Tebrikler! Silinen müşteri: " + musteriAdi + " " + musteriSoyadi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Adi = "Mahmut";
+            musteri1.Soyadi = "Tuncer";
+            musteri1.HesapTuru = "Vadeli";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Adi = "Hayrettin";
+            musteri2.Soyadi = "Bağrıyanık";
+            musteri2.HesapTuru = "Vadesiz";
+
+            Console.WriteLine("-----Müşteri Listeleme-----");
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.List(musteri1);
+            musteriManager.List(musteri2);
+
+            Console.WriteLine("-----Müşteri Ekleme-----");
+
+            musteriManager.Add("Aziziye", "Değişik", "Döviz");
+
+            Console.WriteLine("-----Müşteri Silme-----");
+
+            musteriManager.Delete("Polat", "Alemdar", "Altın");
+
+        }
+    }
+}
